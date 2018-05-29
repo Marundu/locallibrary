b2fe7127bf85c26ae55847dbdfc3589bdd1c02e9
marundu <marundu@gmail.com>
2018-05-30 01:10:40 +0300

    Registered database models on admin site

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Books, BookInstance, Genre, Language
+
+admin.site.register(Author)
+admin.site.register(Books)
+admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
