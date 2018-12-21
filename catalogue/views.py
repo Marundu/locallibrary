ad974d35a437d0b84316e3d1865a7dde15eb3233
chipomwitu <chipomwitu88@gmail.com>
2018-12-21 11:21:45 +0300

    Added url routes and index page

--- catalogue/views.py
+++ catalogue/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalogue.models import Author, Book, BookInstance, Genre
+
+def index(request):
+    
+    '''View function for the home page.'''
+    
+    # Generate counts of some of the main objects
+    
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+    
+    # Available books (status='a')
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    
+    # The all() is implied by default
+    num_authors=Author.objects.count()
+    
+    context={
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+    
+    # Render the HTML template with the data in the context variable
+    return render(request, 'index.html', context=context)
