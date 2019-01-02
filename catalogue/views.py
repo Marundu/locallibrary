559c17b71e46d297aee44f98aaeb643d06cb5ae3
chipomwitu <chipomwitu88@gmail.com>
2019-01-02 21:12:08 +0300

    Added book and author detail views

--- catalogue/views.py
+++ catalogue/views.py
@@ -26,3 +26,17 @@ def index(request):
     
     # Render the HTML template with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model=Book
+
+class AuthorListView(generic.ListView):
+    model=Author
+
+class BookDetailView(generic.DetailView):
+    model=Book
+
+class AuthorDetailView(generic.DetailView):
+    model=Author
